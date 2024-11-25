2abb56b0554634207455d3352c4268b9da1280e5
Claravilera <claravilera@gmail.com>
2024-11-25 23:28:51 +0100

    model, enum, and view ready, need fix app controller

---
+++ src/main/java/ControllerSolarSystem/App.java
@@ -0,0 +1,39 @@
+package ControllerSolarSystem;
+
+import EnumSolarSystem.PlanetsTypes;
+import ModelSolarSystem.planetsModels.PlanetsModels;
+import ViewSolarSystem.viewSolarSystem;
+
+public class App {
+    public static void main(String[] args) {
+        PlanetsModels jupiter = new PlanetsModels(
+            "Jupiter", 79, 1.898e27, 1.43128e15, 139820, 778,
+                PlanetsTypes.GASEOUS, true, 11.86, 0.41);
+
+        PlanetsModels mars = new PlanetsModels(
+                "Mars", 2, 6.417e23, 1.6318e11, 6779, 228,
+                PlanetsTypes.TERRESTRIAL, true,1.88,1.03);
+
+        viewSolarSystem.showMessage("\nPlanet Details:");
+        viewSolarSystem.showMessage("Name: " + jupiter.getName());
+        viewSolarSystem.showMessage("Number of Satellites: " + jupiter.getNumberOfSatellites());
+        viewSolarSystem.showMessage("Mass (kg): " + jupiter.getMass());
+        viewSolarSystem.showMessage("Volume (km³): " + jupiter.getVolume());
+        viewSolarSystem.showMessage("Diameter (km): " + jupiter.getDiameter());
+        viewSolarSystem.showMessage("Average Distance from Sun (million km): " + jupiter.getAverageDistanceFromSun());
+        viewSolarSystem.showMessage("Planet Type: " + jupiter.getType());
+        viewSolarSystem.showMessage(String.format("Density: %.2f kg/km³", jupiter.calculateDensity()));
+        viewSolarSystem.showMessage("Is it an outer planet? " + (jupiter.isOuterPlanet() ? "Yes" : "No"));
+
+        viewSolarSystem.showMessage("\nPlanet Details:");
+        viewSolarSystem.showMessage("Name: " + mars.getName());
+        viewSolarSystem.showMessage("Number of Satellites: " + mars.getNumberOfSatellites());
+        viewSolarSystem.showMessage("Mass (kg): " + mars.getMass());
+        viewSolarSystem.showMessage("Volume (km³): " + mars.getVolume());
+        viewSolarSystem.showMessage("Diameter (km): " + mars.getDiameter());
+        viewSolarSystem.showMessage("Average Distance from Sun (million km): " + mars.getAverageDistanceFromSun());
+        viewSolarSystem.showMessage("Planet Type: " + mars.getType());
+        viewSolarSystem.showMessage(String.format("Density: %.2f kg/km³", mars.calculateDensity()));
+        viewSolarSystem.showMessage("Is it an outer planet? " + (mars.isOuterPlanet() ? "Yes" : "No"));
+    }
+}
---
+++ src/main/java/ModelSolarSystem/planetsModels.java
@@ -0,0 +1,83 @@
+package ModelSolarSystem;
+
+import EnumSolarSystem.PlanetsTypes;
+
+public class planetsModels {
+
+    public class PlanetsModels {
+        private String name;
+        private int numberOfSatellites;
+        private double mass;
+        private double volume;
+        private int diameter;
+        private int averageDistanceFromSun;
+        private PlanetsTypes type;
+        private boolean observableWithNakedEye;
+        private double orbitalPeriod;
+        private double rotationPeriod;
+
+        public PlanetsModels(String name, int numberOfSatellites, double mass, double volume, int diameter,
+                int averageDistanceFromSun, PlanetsTypes type, boolean observableWithNakedEye, double orbitalPeriod,
+                double rotationPeriod) {
+            this.name = name;
+            this.numberOfSatellites = numberOfSatellites;
+            this.mass = mass;
+            this.volume = volume;
+            this.diameter = diameter;
+            this.averageDistanceFromSun = averageDistanceFromSun;
+            this.type = type;
+            this.observableWithNakedEye = observableWithNakedEye;
+            this.orbitalPeriod = orbitalPeriod;
+            this.rotationPeriod = rotationPeriod;
+        }
+
+        public double getOrbitalPeriod() {
+            return orbitalPeriod;
+        }
+
+        public double getRotationPeriod() {
+            return rotationPeriod;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getNumberOfSatellites() {
+            return numberOfSatellites;
+        }
+
+        public double getMass() {
+            return mass;
+        }
+
+        public double getVolume() {
+            return volume;
+        }
+
+        public int getDiameter() {
+            return diameter;
+        }
+
+        public int getAverageDistanceFromSun() {
+            return averageDistanceFromSun;
+        }
+
+        public PlanetsTypes getType() {
+            return type;
+        }
+
+        public boolean isObservableWithNakedEye() {
+            return observableWithNakedEye;
+        }
+
+        public double calculateDensity() {
+            return mass / volume;
+        }
+
+        public boolean isOuterPlanet() {
+            double distanceAU = averageDistanceFromSun * 1e6 / 149597870.0;
+            return distanceAU > 3.4;
+        }
+    }
+}
